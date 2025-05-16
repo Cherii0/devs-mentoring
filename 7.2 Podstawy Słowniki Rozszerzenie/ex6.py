406b3323a77511e3753b8602394618afeb75c823
Cherii0 <zgmnt.main@gmail.com>
2025-05-16 18:57:12 +0200

    Create ex6.py

---
+++ 7.2 Podstawy Słowniki Rozszerzenie/ex6.py
@@ -0,0 +1,8 @@
+# Wyszukaj w zewnętrznych źródłach, jakie obiekty nie mogą być kluczami w słowniku. 
+
+
+# moga byc : typ niezmienny naturalnie str, int, float, bool oraz tuple jezeli kaazdy z ich elementow jest niezmienny czyli zawiera np. same int lub kombinacje int, float, bool, str
+# zmienny typ danych to lista, zbiór, słownik to oznacza ze adres tych obiektow zmienia sie po modyfikacji
+# typ niezmienny posiada niezmienny adres w pamieci
+
+# slowniki potrzebuja niezmiennych typow w kluczach 
