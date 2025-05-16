315e95ae58b0306feeb81e6d3511bbf585e0e43a
Cherii0 <zgmnt.main@gmail.com>
2025-05-16 13:58:23 +0200

    Create ex9.py

---
+++ 6 Podstawy Szkolenie 4 - 6/ex9.py
@@ -0,0 +1,34 @@
+print("koniunkcja")
+
+print("True z operatorem AND tylko gdy wszystkie warunki są spełnione")
+print(True and True, " True and True => True ")
+print(False and False, " False and False => False ")
+print(True and False, " True and False => False ")
+print(False and True, " False and True => False ")
+
+
+print("alternatywa")
+
+print("True z operatorem OR daje True jeżeli conajmniej jeden warunek jest spełniony")
+print(True or True, " True or True => True ")
+print(False or False, " False or False => False ")
+print(True or False, " True or False => True ")
+print(False or True, " False or True => True ")
+
+
+print("Zadanie :")
+
+
+print(True and False)
+print(True and True)
+print(False or False)
+print(False or True)
+
+'''
+
+True and False ---> koniunkcja, wynik : False
+True and True ---> koniunkcja, wynik : True
+False or False ---> alternatywa, wynik : False
+False or True ----> alternatywa, wynik : True
+
+'''
