3a61944d5e3670159af93be425eb2b09b5518db1
Cherii0 <zgmnt.main@gmail.com>
2025-05-17 13:04:26 +0200

    Create ex2.py

---
+++ 9 Podstawy Szkolenie 7 Zadania/ex2.py
@@ -0,0 +1,8 @@
+# Czym się różni argument od parametru funkcji, default arguments od keyword arguments?
+
+# argument funckji to wartosc przekazywana do funkcji podczas jej wywoływania
+# parametr funkcji jest to nazwa zmiennej lokalnej do której ląduje adres przekazywanego argumentu
+
+# default arguments to argumenty które posiadaja domysle wartosci. W przypadku kiedy chcemy nadpisac argument mozemy to zrobic.
+# ketword arguments to argumenty które podczas przekazywania sa zwiazane z nazwa parametru funkcji. Umozliwia to podawanie argumentow do funkcji w dowolnej kolejnosci
+# oraz pozwala upewnic sie ze argument zostanie sparowany z odpowiednim parametrem
