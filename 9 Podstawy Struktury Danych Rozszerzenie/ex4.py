bf8d6fc06b0bcc111fa78893d916bcb6ebb4d930
Cherii0 <zgmnt.main@gmail.com>
2025-05-17 11:32:03 +0200

    Create ex4.py

---
+++ 9 Podstawy Struktury Danych Rozszerzenie/ex4.py
@@ -0,0 +1,19 @@
+dataset = set()
+dataset_res = []
+
+while True:
+
+    data_line = input("podaj pare Miasto opad oddzielone spacjami ")
+
+    if len(data_line) == 0:
+        break
+    else:
+        dataset.add(data_line)
+
+
+
+for elem in dataset.copy():
+    new_elem = elem.replace(" ", " : ")
+    dataset_res.append(new_elem)
+
+print(dataset_res)
