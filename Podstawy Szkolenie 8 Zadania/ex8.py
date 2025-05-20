5854bc6f585e33dfcf72bbd13dffe5dba6839172
Cherii0 <zgmnt.main@gmail.com>
2025-05-20 22:59:15 +0200

    Create ex8.py

---
+++ Podstawy Szkolenie 8 Zadania/ex8.py
@@ -0,0 +1,27 @@
+import json
+
+
+class Solution:
+
+    def __init__(self):
+
+        self.reversed_dict = {}
+
+
+    def convert(self, **kwargs):
+
+        for key, value in kwargs.items():
+            self.reversed_dict.update({value: key})
+
+    def save_result(self):
+        with open("dumped.json", "w") as file:
+            json.dump(self.reversed_dict, file, indent=4)
+
+
+def main():
+    program = Solution()
+    program.convert(a = 1, b = 2, c = 3)
+    program.save_result()
+
+if __name__ == "__main__":
+    main()
