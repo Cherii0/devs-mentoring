68a4b9ad7c45c5f0fcd3f077eb5c519b54549083
Cherii0 <zgmnt.main@gmail.com>
2025-05-20 23:00:25 +0200

    Update ex8.py

--- Podstawy Szkolenie 8 Zadania/ex8.py
+++ Podstawy Szkolenie 8 Zadania/ex8.py
@@ -13,15 +13,15 @@ class Solution:
         for key, value in kwargs.items():
             self.reversed_dict.update({value: key})
 
-    def save_result(self):
-        with open("output.json", "w") as file:
+    def save_result(self, file_path):
+        with open(file_path, "w") as file:
             json.dump(self.reversed_dict, file, indent=4)
 
 
 def main():
     program = Solution()
     program.convert(a = 1, b = 2, c = 3)
-    program.save_result()
+    program.save_result("output.json")
 
 if __name__ == "__main__":
     main()
