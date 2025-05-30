817b25d2ed6baacdb73985c7c6652bc463aa9218
Cherii0 <zygmunt.latyszewicz@outlook.com>
2025-05-30 20:43:22 +0200

    upload ex2.py

---
+++ Dekoratory - Zadania/ex2.py
@@ -0,0 +1,20 @@
+
+
+def outer_lines(func):
+    def inner_func(*, text):
+        line = "*"
+        print(line * 50)
+        func(text = text)
+        print(line * 50)
+    return inner_func
+
+@outer_lines
+def show_text(*, text):
+    print(text)
+
+
+show_text(text = "Total 4 (delta 1), reused 0 (delta 0), pack-reused 0 (from 0)")
+show_text(text ="remote: Resolving deltas: 100% (1/1), completed with 1 local object.")
+show_text(text = "   71c4658..f83cf0c  main -> main.")
+show_text(text = "remote: Resolving deltas: 100% (1/1), completed with 1 local object.")
+show_text(text = r"PS C:\Users\zygmu\PycharmProjects\devs-mentoring>.")
